2c76e219e5306490a9f1745f3a5b74c217c504f1
francisccdn <francisccdn@gmail.com>
2021-11-23 14:47:01 -0300

    stratified sampling

--- build.py
+++ build.py
@@ -1,9 +1,7 @@
 # ESSE ARQUIVO BUILD TEM QUE BAIXAR O MODELO, PRÉ-PROCESSAR OS DADOS E GERAR O ARQUIVO YAML
 from src.downloader import Downloader
 from src.preprocess import Preprocessor
-from utils.build_utils import allocate_split, create_yaml
-from sklearn.model_selection import train_test_split
-import os
+from utils.build_utils import allocate_split, create_yaml, stratified_sampling
 import argparse
 
 
@@ -17,21 +15,23 @@ def get_build_args():
 if __name__ == "__main__":
     downloader = Downloader()
     downloader.run()
+    print("")
 
     processor = Preprocessor()
     processor.run()
+    print("")
 
     create_yaml(train_path="images/train",
                 val_path="images/val",
                 test_path="images/test",
                 labels=processor.labels)
 
-    images_train, images_else, labels_train, labels_else = train_test_split(
-        processor.images_files, processor.labels_files, test_size=0.3)
+    images_train, images_else, labels_train, labels_else = stratified_sampling(
+        processor.labels_files, processor.labels, test_ratio=0.3)
 
-    images_val, images_test, labels_val, labels_test = train_test_split(
-        images_else, labels_else, test_size=10 / len(images_else))
-
+    images_val, images_test, labels_val, labels_test = stratified_sampling(
+        labels_else, processor.labels, test_ratio=10 / len(images_else))
+    
     allocate_split("train", images_train, labels_train)
     allocate_split("val", images_val, labels_val)
     allocate_split("test", images_test, labels_test)
--- utils/build_utils.py
+++ utils/build_utils.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+import random
 
 
 def create_yaml(labels, train_path, val_path, test_path):
@@ -30,6 +31,100 @@ def allocate_split(name, imgs_list, labels_list):
         for img in imgs_list
     ]
     [
-        os.replace(f"datasets/labels/{label}",
-                   f"datasets/labels/{name}/{label}") for label in labels_list
+        os.replace(f"datasets/labels/{label}", f"datasets/labels/{name}/{label}")
+        for label in labels_list
     ]
+
+
+def stratified_sampling(labels_list, labels, test_ratio, verbose=False):
+    # num total de cada label
+    num_label = [0 for _ in labels]
+
+    # num de cada label em cada img
+    num_label_per_img = {file: [0 for _ in labels] for file in labels_list}
+
+    for file in labels_list:
+        with open(f"datasets/labels/{file}", "r") as f:
+            for line in f:
+                label = int(line[0])
+
+                num_label[label] += 1
+                num_label_per_img[file][label] += 1
+
+    # numero alvo de samples no conjunto de teste
+    target_test_samples_per_label = [n * test_ratio for n in num_label]
+    target_train_samples_per_label = [
+        num_label[i] - target_test_samples_per_label[i] for i in range(len(labels))
+    ]
+
+    # sample pool vai ser dividida nas duas listas
+    sample_pool = labels_list
+    train_list = []
+    test_list = []
+
+    # escolhe uma img da lista levando em conta o alvo de samples
+    def weighted_sampling(samples_per_label):
+        random.seed(123)
+
+        sample = random.choices(
+            sample_pool,
+            # peso de cada img é proporcional a
+            # quanto a ela tem * quanto precisamos, para cada label
+            weights=[
+                sum(
+                    [
+                        num_label_per_img[img][label] * samples_per_label[label]
+                        for label in range(len(labels))
+                    ]
+                )
+                for img in sample_pool
+            ],
+        )[0]
+
+        # deduz a qtd de samples dessa img do alvo
+        for label in range(len(labels)):
+            samples_per_label[label] -= num_label_per_img[sample][label]
+
+        sample_pool.remove(sample)
+        return sample
+
+    # escolhe uma img para cada lista até esvaziar a pool
+    while len(sample_pool) > 0:
+
+        # se o alvo do test foi atingido, não escolhe mais nenhuma img pra ele
+        if sum(target_test_samples_per_label) > 0:
+            test_list.append(
+                weighted_sampling(target_test_samples_per_label)
+            )
+
+        train_list.append(
+            weighted_sampling(target_train_samples_per_label)
+        )
+
+    if verbose:
+        # imprime proporção das labels no dataset
+        print("Dataset label proportion:")
+        print(dict(zip(labels, num_label)))
+
+        # imprime qtd de cada label no array train e test
+        num_label_train = [0, 0, 0]
+        for sample in train_list:
+            for label in range(len(labels)):
+                num_label_train[label] += num_label_per_img[sample][label]
+
+        num_label_test = [0, 0, 0]
+        for sample in test_list:
+            for label in range(len(labels)):
+                num_label_test[label] += num_label_per_img[sample][label]
+
+        print("Qty of each in label in train list")
+        print(num_label_train)
+        print("Qty of each in label in test list")
+        print(num_label_test)
+        print("")
+
+    # pega o nome dos arquivos de img a partir dos nomes dos arquivos de label
+    train_list_img = [name.split(".")[0] + ".png" for name in train_list]
+    test_list_img = [name.split(".")[0] + ".png" for name in test_list]
+
+    return train_list_img, test_list_img, train_list, test_list
