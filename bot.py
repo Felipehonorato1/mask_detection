5531867723c9e8d747fdb5581fcf65ab33b8ddd8
francisccdn <francisccdn@gmail.com>
2021-11-27 21:18:33 -0300

    bot additional commands

--- bot.py
+++ bot.py
@@ -20,16 +20,18 @@ class Bot:
         updater = Updater(token="2121563163:AAFfR67OdkZnbsnlIKoi_WMSMcBXm_OvcyE")
         dispatcher = updater.dispatcher
 
-        start_handler = CommandHandler("start", self.start)
-        dispatcher.add_handler(start_handler)
-        register_handler = CommandHandler("register", self.register)
-        dispatcher.add_handler(register_handler)
+        dispatcher.add_handler(CommandHandler("start", self.start))
+        dispatcher.add_handler(CommandHandler("register", self.register))
+        dispatcher.add_handler(CommandHandler("unregister", self.unregister))
+        dispatcher.add_handler(CommandHandler("help", self.help))
+        dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
 
         self.updater = updater
         updater.start_polling()
         updater.idle()
 
     def start(self, update: Update, context: CallbackContext):
+        # TODO mensagem mais explicativa
         response = "Oi!"
 
         context.bot.send_message(chat_id=update.effective_chat.id, text=response)
@@ -37,8 +39,26 @@ class Bot:
     def register(self, update: Update, context: CallbackContext):
         chat = update.effective_chat.id
 
-        self.registered_chats.append(chat)
+        if chat not in self.registered_chats:
+            self.registered_chats.append(chat)
         context.bot.send_message(chat_id=chat, text="Registrado com sucesso!")
+
+    def unregister(self, update: Update, context: CallbackContext):
+        chat = update.effective_chat.id
+
+        self.registered_chats.remove(chat)
+        context.bot.send_message(chat_id=chat, text="Removido do registro.")
+
+    def help(self, update: Update, context: CallbackContext):
+        # TODO mensagem
+        response = "Mensagem explicando a usar o bot."
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+    def unknown(self, update: Update, context: CallbackContext):
+        response = "Desculpe, não sei como te ajudar. Tente /help."
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 
 def init_watcher(bot: Bot):
@@ -52,7 +72,7 @@ def init_watcher(bot: Bot):
                         continue
 
                     bot.updater.bot.send_message(chat_id=chat, text=msg)
-            
+
             sleep(3)
 
     except KeyboardInterrupt:
