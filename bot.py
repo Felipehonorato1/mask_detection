03a0ed684167a03734958be115aa07411b4c8209
francisccdn <francisccdn@gmail.com>
2021-11-27 21:11:10 -0300

    telegram bot watches yolo output

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from time import sleep
+from yolo_interface import check_masked
 from telegram import Update
 from telegram.ext import (
     Updater,
@@ -6,33 +8,26 @@ from telegram.ext import (
     Filters,
     MessageHandler,
 )
-import logging
+import threading
 
 
 class Bot:
     def __init__(self):
         self.registered_chats = []
         self.updater = None
-        self.init_bot()
 
-    def init_bot(self):
+    def run(self):
         updater = Updater(token="2121563163:AAFfR67OdkZnbsnlIKoi_WMSMcBXm_OvcyE")
         dispatcher = updater.dispatcher
-
-        logging.basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=logging.INFO,
-        )
 
         start_handler = CommandHandler("start", self.start)
         dispatcher.add_handler(start_handler)
         register_handler = CommandHandler("register", self.register)
         dispatcher.add_handler(register_handler)
 
+        self.updater = updater
         updater.start_polling()
         updater.idle()
-
-        self.updater = updater
 
     def start(self, update: Update, context: CallbackContext):
         response = "Oi!"
@@ -44,11 +39,31 @@ class Bot:
 
         self.registered_chats.append(chat)
         context.bot.send_message(chat_id=chat, text="Registrado com sucesso!")
-        print(self.registered_chats)
+
+
+def init_watcher(bot: Bot):
+    try:
+        while True:
+            found, msg = check_masked()
+
+            if found:
+                for chat in bot.registered_chats:
+                    if bot.updater is None:
+                        continue
+
+                    bot.updater.bot.send_message(chat_id=chat, text=msg)
+            
+            sleep(3)
+
+    except KeyboardInterrupt:
+        return
 
 
 def main():
     bot = Bot()
+    watcher_thread = threading.Thread(target=init_watcher, args=(bot,), daemon=True)
+    watcher_thread.start()
+    bot.run()
 
 
 if __name__ == "__main__":
---
+++ yolo_interface.py
@@ -0,0 +1,9 @@
+def check_masked():
+    # TODO deve checar o output do yolo e se houver alguém sem mascara
+    # retornar verdadeiro + uma string dizendo quantas pessoas sem mascara
+
+    # TODO retornar também uma captura do momento que estava sem máscara
+    
+    return True, "test message"
+
+    
