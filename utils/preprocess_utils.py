5a4d9328dc01ee24d526ca36177c08ec86f34501
francisccdn <francisccdn@gmail.com>
2021-12-06 12:27:32 -0300

    resize images function

--- utils/preprocess_utils.py
+++ utils/preprocess_utils.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import os
+
+
 def get_yolo(img_width, img_height, x_min, y_min, x_max, y_max):
 
     x_center = (x_max + x_min) / (2 * img_width)
@@ -7,3 +11,11 @@ def get_yolo(img_width, img_height, x_min, y_min, x_max, y_max):
     height = (y_max - y_min) / img_height
 
     return x_center, y_center, width, height
+
+
+def resize_imgs(dirpath, verbose=True):
+    for path in Path(dirpath).glob("**/*"):
+        if path.suffix in [".png", ".jpg", ".jpeg"]:
+            if verbose:
+                print(f"Resizing {str(path)}")
+            os.system(f"convert {str(path)} -resize 640x640 {str(path)}")
